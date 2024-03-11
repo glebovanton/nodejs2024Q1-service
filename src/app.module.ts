3f0ebb1eb52122bd20180612f292e6e796e6d7f2
anton-glebov <anton.glebov@useinsider.com>
2024-03-11 21:46:30 +0300

    feat: change favs import

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
-import { FavouritesModule } from './favourites/favourites.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
   imports: [
@@ -17,7 +17,7 @@ import { FavouritesModule } from './favourites/favourites.module';
     ArtistModule,
     TrackModule,
     AlbumModule,
-    FavouritesModule,
+    FavsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
