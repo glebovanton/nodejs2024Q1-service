28c1bb5007db219c312273012a3acf124319e6e2
anton-glebov <anton.glebov@useinsider.com>
2024-03-11 21:45:05 +0300

    feat: change favorites to favs

---
+++ src/favs/favs.controller.ts
@@ -0,0 +1,59 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { Entity, Fav } from './entities/fav.entity';
+import { FavsService } from './favs.service';
+
+@Controller('favs')
+export class FavsController {
+  private entities = ['track', 'album', 'artist'];
+
+  constructor(private readonly favsService: FavsService) {}
+
+  @Get()
+  findAll(): Fav {
+    return this.favsService.findAll();
+  }
+
+  @Post(':entity/:id')
+  add(
+    @Param('entity') entity: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ): string {
+    if (this.entities.includes(entity)) {
+      this.favsService.add(this.convertToPlural(entity), id);
+      return `${
+        entity[0].toUpperCase + entity.slice(1)
+      } successfully added to favorites`;
+    } else {
+      throw new BadRequestException('Invalid entity');
+    }
+  }
+
+  @Delete(':entity/:id')
+  @HttpCode(204)
+  delete(
+    @Param('entity') entity: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ): string {
+    if (this.entities.includes(entity)) {
+      this.favsService.delete(this.convertToPlural(entity), id);
+      return `${
+        entity[0].toUpperCase + entity.slice(1)
+      } successfully deleted from favorites`;
+    } else {
+      throw new BadRequestException('Invalid entity');
+    }
+  }
+
+  private convertToPlural(entityName: string): Entity {
+    return `${entityName}s` as Entity;
+  }
+}
---
+++ src/favs/favs.service.ts
@@ -0,0 +1,65 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
+import { Entity, Fav } from 'src/favs/entities/fav.entity';
+import { Track } from 'src/track/entities/track.entity';
+
+@Injectable()
+export class FavsService {
+  constructor(private dbService: DatabaseService) {}
+
+  public findAll(): Fav {
+    return this.dbService.favs;
+  }
+
+  public add(entityName: Entity, id: string): void {
+    const entity = this.findEntity(entityName, id);
+
+    const entityInFavs = this.dbService.favs[entityName].find(
+      (entity: Artist | Album | Track) => entity.id === id,
+    );
+
+    if (!entityInFavs) {
+      this.dbService.favs[entityName].push(entity);
+    }
+  }
+
+  public delete(entityName: Entity, id: string): void {
+    const entity = this.dbService.favs[entityName].find(
+      (entity: Artist | Album | Track) => entity.id === id,
+    );
+
+    if (!entity) {
+      throw new NotFoundException(
+        `${
+          entityName[0].toUpperCase + entityName.slice(1)
+        } with this ID not found`,
+      );
+    }
+
+    const entityIndex = this.dbService.favs[entityName].indexOf(entity);
+
+    this.dbService.favs[entityName].splice(entityIndex, 1);
+  }
+
+  private findEntity(entityName: Entity, id: string): Artist | Album | Track {
+    const entity = this.dbService[entityName].find(
+      (entity: Artist | Album | Track) => entity.id === id,
+    );
+
+    if (!entity) {
+      throw new UnprocessableEntityException(
+        `${
+          entityName[0].toUpperCase + entityName.slice(1)
+        } with this ID doesn't exist`,
+      );
+    }
+
+    return entity;
+  }
+}
