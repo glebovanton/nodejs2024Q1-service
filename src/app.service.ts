559b4316a11825d77df97b41199509f725530fa0
anton-glebov <anton.glebov@useinsider.com>
2024-03-19 21:11:31 +0300

    feat: fix AppService message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Welcome to Home Library Service! Go to <a href="http://localhost:${PORT}/doc/">http://localhost:${PORT}/doc/</a>`;
   }
 }
